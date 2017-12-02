387fcb4280d57e8a51883d1f1786953616dfad07
moults31 <moults31@gmail.com>
2017-12-02 12:33:11 -0800

    Remove build directory from git repo

--- build/CMakeFiles/3.8.1/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/moults31/Documents/Engineering/Year4/Term_1/CPEN333/Amaz00m/build/CMakeFiles/3.8.1/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- build/amazoom.build/Debug/ALL_BUILD.build/Script-74E0AB48CE7B404E92E6219A.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/moults31/Documents/Engineering/Year4/Term_1/CPEN333/Amaz00m/build -f /Users/moults31/Documents/Engineering/Year4/Term_1/CPEN333/Amaz00m/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
